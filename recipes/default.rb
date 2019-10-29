c6d30bbe8340187aad78eed5bcb98373d50f3aa2
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-29 14:35:07 +0000

    Created mongodb cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,44 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+#include_recipe 'sc-mongodb::mongodb_org_repo' if node['mongodb']['install_method'] == 'mongodb-org'
+apt_update 'update_sources' do
+  action :update
+end
+
+apt_repository 'mongodb-org' do
+  uri 'http://repo.mongodb.org/apt/ubuntu'
+  distribution 'trusty/mongodb-org/3.2'
+  components ['multiverse']
+  keyserver 'hkp://keyserver.ubuntu.com:80'
+  key 'EA312927'
+  action :add
+end
+
+template '/etc/mongod.conf' do
+  source 'mongod.conf.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
+template '/lib/systemd/system/mongod.service' do
+  source 'mongod.service.erb'
+  mode '0600'
+  owner 'root'
+  group 'root'
+end
+
+package 'mongodb-org' do
+  options '--allow-unauthenticated'
+  action :install
+end
+
+service 'mongod' do
+  action :enable
+end
+
+service 'mongod' do
+  action :start
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,20 +10,23 @@ describe 'mongodb_1::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should install mongodb using a recipe" do
+      expect(chef_run).to install_package 'mongodb-org'
     end
+
+    it "should enable service mongodb" do
+      expect(chef_run).to enable_service 'mongod'
+    end
+
+    it "should start service mongodb" do
+      expect(chef_run).to start_service 'mongod'
+    end
+
   end
 end
